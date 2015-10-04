9b71f276052980cba40db3de84a644acdaa921d3
Javier Vargas <javier1615@gmail.com>
2015-10-04 12:27:22 -0500

    created new menu section

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+    var pull = $('.icon-hamburger');
+    menu = $('.main-nav');
+    menuHeight = menu.height();
+
+   $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.fadeToggle();
+    $('body').toggleClass('hidden-scroll');
+});
+
+$(window).resize(function(){
+        var w = $(window).width();
+        if(w > 992 ) {
+        menu.removeAttr('style');
+        $('body').removeClass('hidden-scroll');
+        }
+    });
+});
